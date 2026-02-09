d76c088fb0bcb27268846f24b7465b272b1071ae
Marc Brinkmann <git@marcbrinkmann.de>
2026-02-09 18:28:17 +0100

    Initial version for `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      fenix,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        toolchain = fenix.packages.${system}.stable.withComponents [
+          "cargo"
+          "clippy"
+          "rust-src"
+          "rustc"
+          "rustfmt"
+        ];
+
+        platform = pkgs.makeRustPlatform {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+        cargoToml = pkgs.lib.importTOML ./Cargo.toml;
+      in
+      {
+        packages.default = platform.buildRustPackage rec {
+          pname = cargoToml.package.name;
+          version = cargoToml.package.version;
+          meta.description = cargoToml.package.description;
+
+          nativeBuildInputs = with pkgs; [
+            makeWrapper
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            openssl
+          ];
+
+          src = pkgs.lib.cleanSource ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          postInstall = ''
+            wrapProgram $out/bin/${pname} \
+              --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.postgresql ]}
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+          buildInputs = with pkgs; [
+            nixfmt-rfc-style
+            postgresql
+          ];
+        };
+      }
+    );
+}
